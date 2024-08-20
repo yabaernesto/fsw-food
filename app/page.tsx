b23618f4f7f730b7d6161d4a2ae1bf2096c2bcb7
Yaba Ernesto <yabaernesto@gmail.com>
2024-08-20 12:42:46 +0100

    feat: add initial search component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 import Header from "./_components/header";
+import Search from "./_components/search";
 
 const Home = () => {
   return (
-    <div>
+    <>
       <Header />
-    </div>
+      <div className="px-5 pt-6">
+        <Search />
+      </div>
+    </>
   );
 };
 
