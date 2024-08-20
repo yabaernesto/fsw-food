57a0f430abc2f58d541605a94a6ecea41b61f9cb
Yaba Ernesto <yabaernesto@gmail.com>
2024-08-20 20:22:29 +0100

    feat: add second promo banner to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,29 @@
-import Image from "next/image";
 import CategoryList from "./_components/category-list";
 import Header from "./_components/header";
 import Search from "./_components/search";
 import ProductList from "./_components/product-list";
 import { Button } from "./_components/ui/button";
 import { ChevronRightIcon } from "lucide-react";
+import { db } from "./_lib/prisma";
+import PromoBanner from "./_components/promo-banner";
 
-const Home = () => {
+const Home = async () => {
+  const products = await db.product.findMany({
+    where: {
+      discountPercentage: {
+        gt: 0,
+      },
+    },
+    take: 10,
+    include: {
+      restaurant: {
+        select: {
+          name: true,
+        },
+      },
+    },
+  });
+
   return (
     <>
       <Header />
@@ -19,14 +36,9 @@ const Home = () => {
       </div>
 
       <div className="px-5 pt-6">
-        <Image
+        <PromoBanner
           src="/promo_banner-01.svg"
           alt="Ate 30% de desconto em pizza!"
-          width={0}
-          height={0}
-          className="h-auto w-full object-contain"
-          sizes="100vw"
-          quality={100}
         />
       </div>
 
@@ -41,7 +53,14 @@ const Home = () => {
             <ChevronRightIcon size={16} />
           </Button>
         </div>
-        <ProductList />
+        <ProductList products={products} />
+      </div>
+
+      <div className="px-5 pt-6">
+        <PromoBanner
+          src="promo_banner-02.svg"
+          alt="A partir de R$17,90 em lanches"
+        />
       </div>
     </>
   );
