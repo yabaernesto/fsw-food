1958a8a802518137b532c400065da5286a7526bb
Yaba Ernesto <yabaernesto@gmail.com>
2024-08-21 23:53:31 +0100

    feat: add product page

---
+++ app/products/_components/product-image.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { Button } from "@/app/_components/ui/button";
+import { Product } from "@prisma/client";
+import { ChevronLeftIcon } from "lucide-react";
+import Image from "next/image";
+
+interface ProductImageProps {
+  product: Pick<Product, "name" | "imageUrl">;
+}
+
+const ProductImage = ({ product }: ProductImageProps) => {
+  const router = useRouter();
+
+  const handleBackClick = () => router.back();
+
+  return (
+    <div className="relative h-[360px] w-full">
+      <Image
+        src={product.imageUrl}
+        alt={product.name}
+        fill
+        className="object-cover"
+      />
+
+      <Button
+        className="absolute left-4 top-4 rounded-full bg-white text-foreground hover:text-white"
+        size="icon"
+        onClick={handleBackClick}
+      >
+        <ChevronLeftIcon />
+      </Button>
+    </div>
+  );
+};
+
+export default ProductImage;
